3980179733269a8660a2a3c0d64fccc4d52571ed
Priscilla Cheng <priscillacheng@Priscillas-MacBook-Pro.local>
2020-12-13 02:35:14 -0500

    Add read file function

---
+++ calendar/day-1/day-1-submission.py
@@ -0,0 +1,11 @@
+# Part 1
+## Find the two entries that sum to 2020; what do you get if you multiply them together?
+
+def read_report():
+	file = open("expense_report_input.txt", "r")
+	print(file.read())
+	
+
+
+if __name__=="__main__":
+	read_report()
