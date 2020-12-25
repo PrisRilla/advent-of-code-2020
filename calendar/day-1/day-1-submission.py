294a2be2c16b1fe1e11d87a941b53608eba06b50
Priscilla Cheng <priscillacheng@Priscillas-MacBook-Pro.local>
2020-12-25 02:06:39 -0500

    Complete part one analysis

--- calendar/day-1/day-1-submission.py
+++ calendar/day-1/day-1-submission.py
@@ -1,11 +1,31 @@
 # Part 1
 ## Find the two entries that sum to 2020; what do you get if you multiply them together?
+import math
+
 
 def read_report():
 	file = open("expense_report_input.txt", "r")
-	print(file.read())
-	
+	nums = [int(x) for x in file.read().split('\n') if x!='']
+	print(nums)
+	return nums
+
+def find_relevant_numbers(nums):
+	"""Return list of relevant numbers that sum to 2020."""
+	ls = []
+	d = {}
+	for n in nums:
+		if n not in d:
+			d[n] = 2020-n
+	for k,v in d.items():
+		if v in nums:
+			if k and v not in ls:
+				ls.extend([k,v])
+	print(ls)
+	return ls		
 
 
 if __name__=="__main__":
-	read_report()
+	nums = read_report()
+	candidates = find_relevant_numbers(nums)
+	prod = math.prod(candidates)
+	print (prod) 
