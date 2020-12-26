32e8a2c90ed401f30bdb925c33f703cd0c3a6d46
Priscilla Cheng <priscillacheng@Priscillas-MacBook-Pro.local>
2020-12-26 00:00:53 -0500

    Write function read input

--- calendar/day-2/day-2-submission.py
+++ calendar/day-2/day-2-submission.py
@@ -1,2 +1,25 @@
 # How many passwords are valid?
 
+
+def read_input():
+	f = open('input.txt','r')
+	file = [row for row in f.read().split('\n')]
+	return file
+
+
+def valid_password(input):
+	"""Checks for valid passwords in ana input list.
+	Input:
+		list: min-max char: password
+	Output:
+		list: password
+	"""
+	return True	
+
+if __name__=="__main__":
+	input = read_input()
+	print(input)
+
+	valid_list = valid_password(input)
+	print(valid_list)
+
