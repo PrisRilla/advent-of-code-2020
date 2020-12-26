bb32f18f08739678e43c81c0da0913d7edf424ea
Priscilla Cheng <priscillacheng@Priscillas-MacBook-Pro.local>
2020-12-26 02:09:10 -0500

    Add files

---
+++ calendar/day-3/day-3-submission.py
@@ -0,0 +1,13 @@
+# Part 1: Starting at the top-left corner of your map and following a slope of right 3 and down 1, how many trees would you encounter?
+
+
+def read_input():
+	f = open('input.txt','r')
+	file = f.read()
+	return file
+
+
+
+if __name__=="__main__":
+	input = read_input()
+	print(input)
