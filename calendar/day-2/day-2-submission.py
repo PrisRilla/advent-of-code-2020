b426e99ed97beb631d2c2be69c571546424d4393
Priscilla Cheng <priscillacheng@Priscillas-MacBook-Pro.local>
2020-12-26 01:01:32 -0500

    Write functions: validate + count validations

--- calendar/day-2/day-2-submission.py
+++ calendar/day-2/day-2-submission.py
@@ -3,23 +3,53 @@
 
 def read_input():
 	f = open('input.txt','r')
-	file = [row for row in f.read().split('\n')]
+	file = [tuple(row.replace(':','').replace('-'," ").split(" ")) for row in f.read().split('\n') if len(row)>0]
 	return file
 
 
-def valid_password(input):
-	"""Checks for valid passwords in ana input list.
+def valid_password(entry):
+	"""Checks if is a valid password for tuple.
 	Input:
-		list: min-max char: password
+		tuple: min, max, letter, password
 	Output:
-		list: password
+		str: password
 	"""
-	return True	
+	min_count = int(entry[0])
+	max_count = int(entry[1])
+	letter = entry[2]
+	password = entry[3]
+	counter = 0
+	
+	for char in password:
+		if char == letter:
+			counter+=1
+
+	if counter >= min_count and counter <= max_count:
+		return password
+	else:
+		return None
+
+
+def count_valid_passwords(input):
+	"""Count number of valid passwords for input list
+	Input:
+		list: tuples (min, max, letter, password)
+	Output:
+		int: number of valid passwords
+	"""
+	ls = []
+	for entry in input:
+		if valid_password(entry) is not None:
+			ls.append(valid_password(entry))
+
+	return len(ls)
+
 
 if __name__=="__main__":
 	input = read_input()
-	print(input)
 
-	valid_list = valid_password(input)
-	print(valid_list)
+	valid = valid_password(input[-3])
+	# print(valid)
 
+	number_valid = count_valid_passwords(input)
+	print(number_valid)
